87d50b9c6c01317148ecb85d6120d1d308626cb8
niko0022 <nikolozishotniasvili@gmail.com>
2026-04-14 20:49:55 +0400

    Wire up routes in main app file

--- vault-relay-frontend/src/App.jsx
+++ vault-relay-frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -9,17 +10,19 @@ import UserSetting from './pages/UserSetting';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/vaults" element={<StorageInfo />} />
-        <Route path="/settings" element={<UserSetting />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/vaults" element={<StorageInfo />} />
+          <Route path="/settings" element={<UserSetting />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
