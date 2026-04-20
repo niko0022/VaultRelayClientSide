346d3d764061663819363840152addf5d7f26aa6
niko0022 <nikolozishotniasvili@gmail.com>
2026-04-20 15:04:16 +0400

    Add protected route to routes

--- vault-relay-frontend/src/App.jsx
+++ vault-relay-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -16,10 +17,27 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/messages" element={<Messages />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/vaults" element={<StorageInfo />} />
-          <Route path="/settings" element={<UserSetting />} />
+          {/* Protected Routes - These components are physically blocked from mounting if unauthenticated */}
+          <Route path="/messages" element={
+            <ProtectedRoute>
+              <Messages />
+            </ProtectedRoute>
+          } />
+          <Route path="/contacts" element={
+            <ProtectedRoute>
+              <Contacts />
+            </ProtectedRoute>
+          } />
+          <Route path="/vaults" element={
+            <ProtectedRoute>
+              <StorageInfo />
+            </ProtectedRoute>
+          } />
+          <Route path="/settings" element={
+            <ProtectedRoute>
+              <UserSetting />
+            </ProtectedRoute>
+          } />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
